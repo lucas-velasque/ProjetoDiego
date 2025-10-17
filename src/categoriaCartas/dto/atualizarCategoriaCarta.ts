d7e4f49cdc3b37a9f22ed6109538e1a4f3febcfa
Lucas Velasque <lucas.velasque@outlook.com>
2025-10-17 02:36:23 -0300

    implementado swagger em categoria cartas

--- src/categoriaCartas/dto/atualizarCategoriaCarta.ts
+++ src/categoriaCartas/dto/atualizarCategoriaCarta.ts
@@ -1,15 +1,32 @@
 import { IsOptional } from "class-validator";
+import { ApiPropertyOptional } from "@nestjs/swagger";
 
 export class atualizarCategoriaCartaDto {
   @IsOptional()
+  @ApiPropertyOptional({
+    description: "Nome da categoria de carta",
+    example: "Categoria A",
+  })
   nome!: string;
 
   @IsOptional()
+  @ApiPropertyOptional({
+    description: "Descrição da categoria de carta",
+    example: "Descrição da categoria A",
+  })
   descricao!: string;
 
   @IsOptional()
+  @ApiPropertyOptional({
+    description: "Tipo da categoria de carta",
+    example: "tipo1",
+  })
   tipo!: string;
 
   @IsOptional()
+  @ApiPropertyOptional({
+    description: "Status da categoria de carta",
+    example: "ativo",
+  })
   status!: string;
 }
